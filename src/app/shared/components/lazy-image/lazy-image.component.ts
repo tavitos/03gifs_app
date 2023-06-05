4c1674e9cf3736300c8e076b144177628081eab7
tavitos <lesamaoyabi@gmail.com>
2023-06-05 13:10:15 -0600

    Sección 8 Image Loader curso angular 2023 Fernando Herrera

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html'
+})
+export class LazyImageComponent implements OnInit {
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if( !this.url ) throw new Error('URL property is required.');
+  }
+
+  onLoad(){
+    // console.log('Image loaded');
+    // setTimeout(() => {
+    //   this.hasLoaded = true;
+    // }, 1000);
+    this.hasLoaded = true;
+  }
+
+}
