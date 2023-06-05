9f43c4407941a6f51648ab39dba809e7e13f8104
usuario <usuario@MacBook-Pro-de-usuario.local>
2023-06-04 23:33:40 -0600

    Resolución de la tarea y concepto de LocalStorage guardar y leer.

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,11 @@ export class GifsService {
     private apiKey:       string = '3y1SZ2oqrOYyTRk2vqburJo5wYd1aAMJ';
     private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
 
-    constructor( private http: HttpClient) { } /** Inyección del servicio HttpClient suministrado por HttpClientModule importado en app.module.ts */
+    constructor( private http: HttpClient) { /** Inyección del servicio HttpClient suministrado por HttpClientModule importado en app.module.ts */
+        this.loadLocalStorage();
+        console.log('Gifs Service Ready');
+        
+     } 
 
     get tagsHistory(){
         return [...this._tagsHistory];
@@ -26,6 +30,7 @@ export class GifsService {
 
         this._tagsHistory.unshift(tag);
         this._tagsHistory = this._tagsHistory.splice(0,10);
+        this.saveLocalStorage();
     }
 
     // Ejemplo con fetch
@@ -45,7 +50,19 @@ export class GifsService {
     //     // console.log(data);
         
     // }
+
+    saveLocalStorage():void {
+        localStorage.setItem('history', JSON.stringify(this._tagsHistory) );
+    }
     
+    private loadLocalStorage():void {
+        if( !localStorage.getItem('history') ) return;
+        this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+        // console.log(this._tagsHistory);
+        if(this._tagsHistory.length === 0) return;
+        this.searchTag(this._tagsHistory[0]);
+    }
+
     searchTag( tag: string ):void  {
         if( tag.length === 0) return;
         this.organizeHistory(tag);
